2ca43160e8b39f320db06da422f4b535ea9a95c1
achaa <hafshahazzahraa@gmail.com>
2024-12-10 15:40:19 +0700

    bismillah

---
+++ Test/TestViewRemoveTodoList.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "../Model/TodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+
+addTodoList("Belajar PHP");
+addTodoList("Belajar PHP OOP");
+addTodoList("Belajar PHP Web");
+addTodoList("Belajar MySql");
+addTodoList("Belajar Laravel");
+
+showTodoList();
+
+viewRemoveTodoList();
+
+showTodoList();
